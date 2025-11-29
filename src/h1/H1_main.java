674eb7b2f4fca68fbc240d9b8a5419187472dce7
Benn Tschierschke <benn@Mac.fritz.box>
2025-11-29 23:10:17 +0100

    Hausaufgabe 1: isMirrorArray 2

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -7,4 +7,17 @@ public class H1_main {
 
 	}
 
+	public static boolean isMirrorArray(int[] a, int[] b) {
+
+		if (a.length != b.length) {
+			return false;
+		}
+
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] != b[b.length - 1 - i]) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
