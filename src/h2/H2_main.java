d029d5b7f1b8fda122cf40107e95af55773586da
Benn Tschierschke <benn@Mac.fritz.box>
2025-11-29 23:54:12 +0100

    Hausaufgabe 2: compareArrays 2

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -7,4 +7,18 @@ public class H2_main {
 
 	}
 
+	public static boolean compareArrays(int[] a, int[] b) {
+		if (a.length != b.length) {
+			return false;
+		} else {
+			for (int i = 0; i < a.length; i++) {
+				if (a[i] != b[i]) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+	}
+
 }
