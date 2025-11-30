0483efcd0a7d63cd234ef9cd257adef87beb3f66
Benn Tschierschke <benn@Mac.fritz.box>
2025-11-30 12:32:41 +0100

    Hausaufgabe 3: compareArraysVal

---
+++ src/h3/H3_main.java
@@ -0,0 +1,34 @@
+package h3;
+
+public class H3_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static boolean compareArraysVal(int[] a, int[] b) {
+        
+		if(a.length != b.length) return false;
+
+        for(int i = 0; i < a.length; i++) {
+            int countA = 0, countB = 0;
+
+            for(int j = 0; j < a.length; j++) {
+                if(a[j] == a[i]) {
+                	countA++;
+                }
+            }
+
+            for(int j = 0; j < b.length; j++) {
+                if(b[j] == a[i]) {
+                	countB++;
+                }
+            }
+
+            if(countA != countB) return false;
+        }
+
+        return true; 
+    }
+
+}
